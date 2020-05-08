607b6907b630d85ad7c01415ab34907f70b78970
Tuncay72 <64120952+Tuncay72@users.noreply.github.com>
2020-05-08 14:46:51 -0500

    Update CreateChannel.java

--- src/day01/CreateChannel.java
+++ src/day01/CreateChannel.java
@@ -3,5 +3,7 @@ package day01;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("Creating channel function is working");
+        
+        // hi everyone
     }
 }
